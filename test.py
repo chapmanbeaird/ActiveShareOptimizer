49b0b751862e058a7f9c761a7fb76702dbd19e3c
Chapman Beaird <chapmanbeaird@Chapmans-MacBook-Pro-4.local>
2025-05-07 22:15:47 -0600

    .

--- test.py
+++
@@ -1,8 +0,0 @@
-from pulp import LpProblem, LpMinimize, LpVariable, LpStatus, COIN_CMD
-
-solver = COIN_CMD(path="/opt/homebrew/bin/cbc", msg=True)
-prob = LpProblem("test", LpMinimize)
-x = LpVariable("x", 0, 10)
-prob += x
-prob.solve(solver)
-print(LpStatus[prob.status])
